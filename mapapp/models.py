3781ba54db7f16683ba1076ad3bb6ceddb38a418
Son Tieu <Son Tieu@my-computer>
2011-08-15 23:24:26 +0700

    I have just created 4 models: 	+ District 	+ NguoiTiepCan 	+ KindOfConstruction 	+ Construction

--- mapapp/models.py
+++ mapapp/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class testing(models.Model):
-    test_demo = models.CharField()
+class District(models.Model):
+    name        = models.CharField(50)
+    #location   = 
+    description = models.TextField()
+   
+class NguoiTiepCan(models.Model):
+    link_icon = models.ImageField()
+    
+class KindOfConstruction(models.Model):
+    name = models.CharField(100)
+
+class Construction(models.Model):   
+    district           = models.ForeignKey(District)
+    loai_ct            = models.ForeignKey(KindOfConstruction)
+    location           = models.CharField()
+    link_image         = models.ImageField()
+    description_detail = models.TextField()
+    description_other  = models.TextField()
+    nguoi_tc           = models.ManyToManyField(NguoiTiepCan)
+
+
+    
