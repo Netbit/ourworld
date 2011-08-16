2642bb037c70c3f96133b6bbbb9dd79299b6645f
hongphi <hongphi.math@gmail.com>
2011-08-16 21:31:07 +0700

    add administration for model

---
+++ mapapp/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from mapapp.models import District, KindOfConstruction, Construction
+
+
+class AdminDistrict(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    list_filter = ('name',)
+    search_fields = ('name',)
+    
+class AdminKindOfConstruction(admin.ModelAdmin):
+    list_display = ['name',]
+    list_filter = ('name',)
+    search_fields = ('name',)
+    
+class AdminConstruction(admin.ModelAdmin):
+    list_display = ['name', 'loai_ct', 'district',]
+    list_filter = ('name', 'loai_ct', 'district')
+    search_fields = ('name','loai_ct', 'district')
+    
+
+admin.site.register(District, AdminDistrict)
+admin.site.register(KindOfConstruction, AdminKindOfConstruction)
+admin.site.register(Construction, AdminConstruction)
