8aa38c733d6350a7a33ebff727b856195324389f
hongphi <hongphi.math@gmail.com>
2011-08-16 21:10:58 +0700

    add properties for fields

--- mapapp/models.py
+++ mapapp/models.py
@@ -2,24 +2,38 @@ from django.db import models
 
 # Create your models here.
 class District(models.Model):
-    name        = models.CharField(50)
-    location    = models.CharField()
-    description = models.TextField()
+    name        = models.CharField(max_length = 100)
+    location    = models.CharField(max_length = 30)
+    description = models.TextField(blank = True)
+    
+    def __unicode__(self):
+        return self.name
    
 class NguoiTiepCan(models.Model):
-    link_icon = models.ImageField()
+    name      = models.CharField(max_length = 100)
+    link_icon = models.ImageField(upload_to = 'images/icon')
+    
+    def __unicode__(self):
+        return self.name
     
 class KindOfConstruction(models.Model):
-    name = models.CharField(100)
+    name = models.CharField(max_lenght = 100)
+    
+    def __unicode__(self):
+        return self.name
 
 class Construction(models.Model):   
     district           = models.ForeignKey(District)
     loai_ct            = models.ForeignKey(KindOfConstruction)
-    location           = models.CharField()
-    link_image         = models.ImageField()
-    description_detail = models.TextField()
-    description_other  = models.TextField()
+    name               = models.CharField(max_length = 100)
+    location           = models.CharField(max_length = 30, blank = True)
+    link_image         = models.ImageField(upload_to = 'images/place')
+    description_detail = models.TextField(blank = True)
+    description_other  = models.TextField(blank = True)
     nguoi_tc           = models.ManyToManyField(NguoiTiepCan)
+    
+    def __unicode__(self):
+        return self.name
 
 
     
