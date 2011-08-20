6ca896ec3f733c5c9ad818b93a8becbe74cbcfee
hongphi <hongphi.math@gmail.com>
2011-08-20 23:37:47 +0700

    add url home

--- urls.py
+++ urls.py
@@ -5,8 +5,9 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('mapapp.views',
    
+    url(r'^$', 'home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
