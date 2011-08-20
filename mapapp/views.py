69a75e7fdd71c069c920a614feea1e42f95c0b74
hongphi <hongphi.math@gmail.com>
2011-08-20 23:17:00 +0700

    add view home

--- mapapp/views.py
+++ mapapp/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def home(request):
+    
+    return render_to_response('index.html', {}, 
+                              context_instance = RequestContext(request))
