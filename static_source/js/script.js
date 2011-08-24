edbccbefb73654cd944a63ae9bf7ea5706a0054e
hongphi <hongphi.math@gmail.com>
2011-08-24 22:28:58 +0700

    add search path

---
+++ static_source/js/script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $('.point').click(function() {
+    	$('.search-path').css('display','none');
+    	$('.search-place').css('display','block');
+    	$('.box').css('width', '332px');
+	});
+    
+    $('.path').click(function() {
+    	$('.search-place').css('display','none');
+    	$('.search-path').css('display', 'block');
+    	$('.box').css('width', '570px');
+	});
+    
+    $('.swap').click(function() {
+    	var temp = $('#a').val();
+    	$('#a').val($('#b').val());
+    	$('#b').val(temp);
+    });
+});
