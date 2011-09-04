5ebd35a04920b6c616f84691df7eed68896bc94c
hongphi <hongphi.math@gmail.com>
2011-09-04 21:26:48 +0700

    find path by sontieu

--- static_source/js/script.js
+++ static_source/js/script.js
@@ -1,10 +1,10 @@
 
-$(document).ready(function(){
+var map;
 
-  	var address; 
-  	var geocoder;
-  	var map;
-  	
+function initialize() {
+	var address;
+	var geocoder;
+	
 	address = "Ho Chi Minh City";
  	geocoder = new google.maps.Geocoder();
 	geocoder.geocode({'address': address}, function(results, status) {
@@ -19,22 +19,56 @@ $(document).ready(function(){
         	alert("Geocode was not successful for the following reason: " + status);
       	}
 	});
-	
-  	$('#search_place').click(function() {
-	    address = document.getElementById("p").value;
-	    geocoder.geocode({'address': address}, function(results, status) {
-	    	if (status == google.maps.GeocoderStatus.OK) {
-	    		map.setCenter(results[0].geometry.location);
-	    		var marker = new google.maps.Marker({
-	    							map: map,
-	    							position: results[0].geometry.location
-	    							});	    		
-	    	} else {
-	    		alert("Geocode was not successful for the following reason: " + status);
-	    	}   
-	    });
-	});
+}
 
+
+
+function search_place(address) {
+	var geocoder;
+	
+	geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+								map: map,
+								position: results[0].geometry.location
+								});	    		
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}   
+	});
+}
+
+
+$(document).ready(function(){
+	initialize();
+	
+	 $('#search_place').click(function() {
+	 		var address;
+	 		
+	    	address = document.getElementById("p").value;
+	    	search_place(address);	    
+	 });
+
+	 $('#search_path').click(function() {
+	    var start   = document.getElementById("a").value;
+	    var end     = document.getElementById("b").value;
+	    var request = {
+		    origin:start,
+		    destination:end,
+		    travelMode: google.maps.TravelMode.DRIVING
+		};
+		var directionsService = new google.maps.DirectionsService();
+		var directionsDisplay = new google.maps.DirectionsRenderer();
+		directionsDisplay.setMap(map);
+		
+		directionsService.route(request, function(result, status) {
+			 if (status == google.maps.DirectionsStatus.OK) {
+	 			 directionsDisplay.setDirections(result);
+			}
+		});
+	});
   	
     $('.point').click(function() {
     	$('.search-path').css('display','none');
