41f80d5fa50868f57ecdc1ef8646250ec007e166
hongphi <hongphi.math@gmail.com>
2011-09-10 23:05:21 +0700

    add script processing for information box

--- static_source/js/script.js
+++ static_source/js/script.js
@@ -1,18 +1,24 @@
 var map;
+var markersArray = [];
+var destinationIcon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=D|FF0000|000000";
+var originIcon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=O|FFFF00|000000";
+var geocoder;
+var bounds;
 
-function get_info_of_place()
-{
+function get_info_of_place() {
 	var contentString = 'Hello';
-	var infowindow = new google.maps.InfoWindow({content: contentString});
+	var infowindow = new google.maps.InfoWindow({
+		content : contentString
+	});
 	return infowindow;
 }
 
 function initialize() {
 	var address;
-	var geocoder;
 
 	address = "Ho Chi Minh City";
 	geocoder = new google.maps.Geocoder();
+	bounds = new google.maps.LatLngBounds();
 	geocoder.geocode({
 		'address' : address
 	}, function(results, status) {
@@ -32,11 +38,10 @@ function initialize() {
 }
 
 function search_place(address) {
-	var geocoder;
-	var marker
+
+	var marker;
 	var infowindow;
-	
-	geocoder = new google.maps.Geocoder();
+
 	geocoder.geocode({
 		'address' : address
 	}, function(results, status) {
@@ -49,7 +54,7 @@ function search_place(address) {
 			infowindow = get_info_of_place();
 			google.maps.event.addListener(marker, 'click', function() {
 				infowindow.open(map, marker);
-			});	
+			});
 		} else {
 			alert("Geocode was not successful for the following reason: "
 					+ status);
@@ -81,6 +86,80 @@ function attachInstructionText(marker, text) {
 	});
 }
 
+function calculateDistances(origin, destination) {
+	var service = new google.maps.DistanceMatrixService();
+	service.getDistanceMatrix({
+		origins : [ origin, ],
+		destinations : [ destination, ],
+		travelMode : google.maps.TravelMode.DRIVING,
+		unitSystem : google.maps.UnitSystem.METRIC,
+		avoidHighways : false,
+		avoidTolls : false
+	}, callback);
+}
+
+function callback(response, status) {
+	if (status != google.maps.DistanceMatrixStatus.OK) {
+		alert('Error was: ' + status);
+	} else {
+		var origins = response.originAddresses;
+		var destinations = response.destinationAddresses;
+		var outputDiv = document.getElementById('content');
+		outputDiv.innerHTML = '';
+		deleteOverlays();
+
+		for ( var i = 0; i < origins.length; i++) {
+			var results = response.rows[i].elements;
+			addMarker(origins[i], false);
+			for ( var j = 0; j < results.length; j++) {
+				addMarker(destinations[j], true);
+				outputDiv.innerHTML += "<b>" + gettext("From") + "</b>: " + origins[i] + "<br>" +
+									   "<b>" + gettext("To") + "</b>: " + destinations[j] + "<br>" +
+									   "<b>" + gettext("Distance") + "</b>: " + results[j].distance.text + "<br>" +
+									   "<b>" + gettext("Time") + "</b>: " + results[j].duration.text + " " +
+									   gettext("(by car)");
+			}
+			$('#box').fadeIn('slow').show();
+			$('#show-box').hide();
+		}
+	}
+}
+
+function addMarker(location, isDestination) {
+	var icon;
+	if (isDestination) {
+		icon = destinationIcon;
+	} else {
+		icon = originIcon;
+	}
+	geocoder.geocode({
+		'address' : location
+	}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			bounds.extend(results[0].geometry.location);
+			map.fitBounds(bounds);
+			var marker = new google.maps.Marker({
+				map : map,
+				position : results[0].geometry.location,
+				icon : icon
+			});
+			markersArray.push(marker);
+		} else {
+			alert("Geocode was not successful for the following reason: "
+					+ status);
+		}
+	});
+}
+
+function deleteOverlays() {
+	if (markersArray) {
+		for (i in markersArray) {
+			markersArray[i].setMap(null);
+		}
+		markersArray.length = 0;
+	}
+}
+
 $(document).ready(function() {
 	initialize();
 
@@ -106,6 +185,7 @@ $(document).ready(function() {
 		directionsService.route(request, function(result, status) {
 			if (status == google.maps.DirectionsStatus.OK) {
 				directionsDisplay.setDirections(result);
+				calculateDistances(start, end);
 				showSteps(result);
 			}
 		});
@@ -114,13 +194,13 @@ $(document).ready(function() {
 	$('.point').click(function() {
 		$('.search-path').css('display', 'none');
 		$('.search-place').css('display', 'block');
-		$('.box').css('width', '332px');
+		$('.box-search').css('width', '332px');
 	});
 
 	$('.path').click(function() {
 		$('.search-place').css('display', 'none');
 		$('.search-path').css('display', 'block');
-		$('.box').css('width', '570px');
+		$('.box-search').css('width', '570px');
 	});
 
 	$('.swap').click(function() {
@@ -161,5 +241,15 @@ $(document).ready(function() {
 		} catch (e) {
 			alert(e);
 		}
+	});
+
+	$('#hidden-popup').click(function() {
+		$('#box').fadeOut('slow').hide();
+		$('#show-box').show();
+	});
+
+	$('#show-popup').click(function() {
+		$('#show-box').hide();
+		$('#box').fadeIn('slow').show();
 	});
 });
