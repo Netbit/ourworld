e9406467a445897a9d58c8fc4cff981151396df2
hongphi <hongphi.math@gmail.com>
2011-09-10 23:06:31 +0700

    updated urls

--- urls.py
+++ urls.py
@@ -5,6 +5,10 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+js_info_dict = {
+    'packages': ('mapapp',),
+}
+
 urlpatterns = patterns('',
    
     url(r'^', include('mapapp.urls')),
@@ -13,4 +17,5 @@ urlpatterns = patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 )
