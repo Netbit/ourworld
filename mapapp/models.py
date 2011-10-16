27c9a8da5b4849176605d3040039c08c5c9125c3
hongphi <hongphi.math@gmail.com>
2011-10-16 23:43:26 +0700

    add property for model

--- mapapp/models.py
+++ mapapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _, activate
+from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
