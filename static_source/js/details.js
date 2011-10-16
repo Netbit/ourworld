158f71ce9d0dd9fe03eb118087661a39bb362631
hongphi <hongphi.math@gmail.com>
2011-10-16 23:45:10 +0700

    add script for popup page

---
+++ static_source/js/details.js
@@ -0,0 +1,6 @@
+function show() {
+	var form = document.getElementById("comment");
+	var insert = document.getElementById("insert-form");
+	insert.style.visibility = 'hidden';
+	form.style.visibility = 'visible';	
+}
