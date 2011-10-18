710e679906981af2e76c52d937ea49ed91020804
hongphi <hongphi.math@gmail.com>
2011-10-18 21:34:14 +0700

    Fixed the comment form is overflowed

--- static_source/js/details.js
+++ static_source/js/details.js
@@ -1,6 +1,8 @@
 function show() {
 	var form = document.getElementById("comment");
 	var insert = document.getElementById("insert-form");
+	var base = document.getElementById("comment-form");
 	insert.style.visibility = 'hidden';
-	form.style.visibility = 'visible';	
+	form.style.visibility = 'visible';
+	base.style.height = '270px';
 }
