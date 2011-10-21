8aae76df31c14323811c6d868d66ac3b332647b2
hongphi <hongphi.math@gmail.com>
2011-10-21 23:20:56 +0700

    add nickname filter

--- mapapp/templatetags/tagslib.py
+++ mapapp/templatetags/tagslib.py
@@ -13,3 +13,7 @@ def display_comments(object_id):
     con = Construction.objects.get(id__exact=object_id)
     comments = Comment.objects.filter(construction = con).order_by('-comment_date')
     return { 'comments': comments }
+
+@register.filter
+def nickname(email):
+    return email.split('@')[0]
