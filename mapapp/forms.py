796de1b1a48d94e8329e423e3f9b27980ddace75
hongphi <hongphi.math@gmail.com>
2011-10-21 23:22:03 +0700

    update form

--- mapapp/forms.py
+++ mapapp/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 class CommentForm(forms.Form): 
-    email = forms.EmailField(label = _('Email'))
-    content = forms.CharField(label = _('Content'), widget=forms.Textarea())   
+    email = forms.EmailField(label = _('Email'), widget=forms.TextInput(attrs={'placeholder': 'john.doe@email.com'}))
+    content = forms.CharField(label = _('Content'), widget=forms.Textarea(attrs={'placeholder': _('Your comment here...')}))   
     
