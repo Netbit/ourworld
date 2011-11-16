1eb2238b2cdd7bc2c4f1e7cf9360e2efb4612abc
hongphi <hongphi.math@gmail.com>
2011-11-16 22:56:02 +0700

    add js for admin map

---
+++ static_source/js/admin_map.js
@@ -0,0 +1,28 @@
+function initialize() {
+	var address;
+
+	address = "Ho Chi Minh City";
+	geocoder = new google.maps.Geocoder();
+	bounds = new google.maps.LatLngBounds();
+	geocoder.geocode({
+		'address' : address
+	}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			var myOptions = {
+				zoom : 20,
+				center : results[0].geometry.location,
+				mapTypeId : google.maps.MapTypeId.ROADMAP
+			};
+			map = new google.maps.Map(document.getElementById("my_map"),
+					myOptions);
+		} else {
+			alert("Geocode was not successful for the following reason: "
+					+ status);
+		}
+	});
+}
+
+$(document).ready(function() {	
+	$('.location').append("<div style='width: 550px; height:400px' id='my_map'></div>");
+	initialize();
+});
