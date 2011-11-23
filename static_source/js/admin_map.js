781079818af5537e4645d1d4f7419af40eedd8ce
Son Tieu <tieuthanhson.lpt@gmail.com>
2011-11-23 23:28:41 +0700

    Set location in file admin_map.js (cont.)

--- static_source/js/admin_map.js
+++ static_source/js/admin_map.js
@@ -20,65 +20,67 @@ function initialize() {
 			};
 			map = new google.maps.Map(document.getElementById("my_map"),
 					myOptions);
+			location = document.getElementById('id_location').value;
+	
+			if ("" != location) {
+				if (null != marker) {
+					marker.setMap(null);
+				}
+				location = location.replace("(","[");
+				location = location.replace(")","]");
+				location = eval(location);
+				pos = new google.maps.LatLng(location[0], location[1]);
+				map.setCenter(pos);
+				marker = new google.maps.Marker({
+					map : map,
+					draggable:true,
+					position : pos,
+				});	
+				google.maps.event.addListener(marker, 'drag', function() {	
+					document.getElementById('id_location').value = "(" + marker.position.Pa + "," + marker.position.Qa + ")";
+				});	
+			}			
 		} else {
 			alert("Geocode was not successful for the following reason: "
 					+ status);
 		}
 	});
-	
-	location = document.getElementById('id_location').value;
-	
-	if ("" != location) {
-		if (null != marker) {
-			marker.setMap(null);
-		}
-		location = location.replace("(","[");
-		location = location.replace(")","]");
-		location = eval(location);
-		pos = new google.maps.LatLng(location[0], location[1]);
-		map.setCenter(pos);
-		marker = new google.maps.Marker({
-			map : map,
-			draggable:true,
-			position : pos,
-		});		
-	}
 }
 
 function search_place()
 {
-//	var address;
-//	var district;
-//		
-//		district = document.getElementById('id_district').value;
-//		address  = document.getElementById('id_number_or_alley').value + " "
-//				   + $("#id_street option:selected").text() + " "
-//				   + document.getElementById('id_ward').value + " "
-//				   + district + " Ho Chi Minh" ;
-//		if (null != marker) {
-//			marker.setMap(null);
-//		}
-//		geocoder = new google.maps.Geocoder();
-//		geocoder.geocode({
-//			'address' : address
-//		}, function(results, status) {
-//			if (status == google.maps.GeocoderStatus.OK) {
-//				map.setCenter(results[0].geometry.location);
-//				document.getElementById('id_location').value = "(" + results[0].geometry.location.Pa + "," + results[0].geometry.location.Qa + ")";
-//				marker = new google.maps.Marker({
-//					map : map,
-//	    			draggable:true,
-//					position : results[0].geometry.location,
-//					address : results[0].formatted_address
-//				});
-//				google.maps.event.addListener(marker, 'drag', function() {	
-//					document.getElementById('id_location').value = "(" + marker.position.Pa + "," + marker.position.Qa + ")";
-//				});
-//			} else {
-//				alert("Geocode was not successful for the following reason: "
-//						+ status);
-//			}
-//		});
+	var address;
+	var district;
+		
+	district = document.getElementById('id_district').value;
+	address  = document.getElementById('id_number_or_alley').value + " "
+			   + $("#id_street option:selected").text() + " "
+			   + document.getElementById('id_ward').value + " "
+			   + district + " Ho Chi Minh" ;
+	if (null != marker) {
+		marker.setMap(null);
+	}
+	geocoder = new google.maps.Geocoder();
+	geocoder.geocode({
+		'address' : address
+	}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			document.getElementById('id_location').value = "(" + results[0].geometry.location.Pa + "," + results[0].geometry.location.Qa + ")";
+			marker = new google.maps.Marker({
+				map : map,
+    			draggable:true,
+				position : results[0].geometry.location,
+				address : results[0].formatted_address
+			});
+			google.maps.event.addListener(marker, 'drag', function() {	
+				document.getElementById('id_location').value = "(" + marker.position.Pa + "," + marker.position.Qa + ")";
+			});
+		} else {
+			alert("Geocode was not successful for the following reason: "
+					+ status);
+		}
+	});
 }
 
 $(document).ready(function() {
